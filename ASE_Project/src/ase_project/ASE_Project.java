cde059ebc92d4d040c30d26a9e89e2101d0e16f5
Mahmoud Osama <mahmoud.01222956504@gmail.com>
2026-04-22 01:43:21 +0200

    v1.0

---
+++ ASE_Project/src/ase_project/ASE_Project.java
@@ -0,0 +1,20 @@
+
+package ase_project;
+
+/**
+ *
+ * @author Mahmoud
+ */
+public class ASE_Project {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Login_Form login = new Login_Form();
+        login.setLocation(400,400);
+        login.setVisible(true);
+        login.setResizable(false);
+    }
+    
+}
